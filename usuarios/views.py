e9b4476261f862bd133394961376fc3e23c31687
Matheus Oliveira <matheuseu41@gmail.com>
2023-11-08 22:18:10 -0300

    Organizando arquivos estáticos e página de consulta e edição de usuários

--- usuarios/views.py
+++ usuarios/views.py
@@ -9,7 +9,8 @@ from django.contrib import auth
 @has_permission_decorator('cadastrar_vendedor')
 def cadastrar_vendedor(request):
     if request.method == "GET":
-        return render(request, 'cadastrar_vendedor.html')
+        vendedores = Users.objects.filter(cargo="V")
+        return render(request, 'cadastrar_vendedor.html', {'vendedores': vendedores})
     if request.method == "POST":
         email = request.POST.get('email')
         senha = request.POST.get('senha')
